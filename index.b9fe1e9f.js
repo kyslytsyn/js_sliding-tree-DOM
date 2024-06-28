c95f2c506cacc2e2f13bcf16fe9666d32dc19db8
Oleksii Kyslytsyn <kislitsyn.aleksey@gmail.com>
2024-06-28 10:45:59 +0300

    make build

---
+++ index.b9fe1e9f.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".tree");document.querySelectorAll("li").forEach(function(e){var n=document.createElement("span");e.prepend(n),n.prepend(n.nextSibling)}),e.addEventListener("click",function(e){if("SPAN"===e.target.tagName){var n=e.target.nextSibling;n&&(n.hidden=!n.hidden)}});
+//# sourceMappingURL=index.b9fe1e9f.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,16 @@
+"use strict";
+const list = document.querySelector(".tree");
+const items = document.querySelectorAll("li");
+items.forEach((item)=>{
+    const wrap = document.createElement("span");
+    item.prepend(wrap);
+    wrap.prepend(wrap.nextSibling);
+});
+list.addEventListener("click", (action)=>{
+    if (action.target.tagName === "SPAN") {
+        const el = action.target.nextSibling;
+        if (el) el.hidden = !el.hidden;
+    }
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
